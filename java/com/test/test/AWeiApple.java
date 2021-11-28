1e8fcc2ab8df19892fa0ca20ed49c98d4ed7dd40
70293039 <kangming06@hotmail.com>
2021-11-28 14:56:16 +0800

    add JieGeBlock

--- java/com/test/test/AWeiApple.java
+++ java/com/test/test/AWeiApple.java
@@ -6,11 +6,15 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
+import java.util.function.Supplier;
+
 public class AWeiApple extends Item {
-    private static EffectInstance effectInstance = new EffectInstance(Effects.STRENGTH, 10 * 20, 114);
+    private static Supplier<EffectInstance> effectInstance = () -> new EffectInstance(Effects.STRENGTH, 10 * 20, 114);
+//    private static EffectInstance effectInstance = new EffectInstance(Effects.STRENGTH, 10 * 20, 114);
     private static Food food = (new Food.Builder())
             .saturation(10)
             .hunger(20)
+//            .effect(effectInstance.getEffectInstance(), 1)
             .effect(effectInstance, 1)
             .build();
 
